7ab5a38e0f8128ad7d6e83aa4efdcc81d37bf9cd
awadieudone <dieudonneawa7@gmail.com>
2019-09-19 21:45:59 +0100

    Updates

--- src/reducers/countriesReducer.js
+++
@@ -1,8 +0,0 @@
-export default (state = [], action) => {
-  switch (action.type) {
-    case 'FETCH_COUNTRIES':
-      return action.payload;
-    default:
-      return state;
-  }
-};
--- src/reducers/holidayReducer.js
+++
@@ -1,8 +0,0 @@
-export default (state = [], action) => {
-  switch (action.type) {
-    case 'FETCH_HOLIDAY':
-      return [...state, action.payload];
-    default:
-      return state;
-  }
-};
